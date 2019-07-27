240f93ef61234f84c871be8a81f55e721af666e7
SASSOUI Abdel <sassouiabd@gmail.com>
2019-07-27 23:36:38 +0200

    App basis

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component
+{
+  constructor() {
+    super();
+    this.state = {
+        userInput: '',
+        items: []
+    };
+}
+
+onChange(event) {
+    this.setState({
+        userInput: event.target.value
+    });
+}
+
+addTodo(event) {
+    event.preventDefault();
+    this.setState({
+        userInput: '',
+        items: [...this.state.items, this.state.userInput]
+    });
+}
+
+deleteTodo(item) {
+    // no event 
+    // pass the item to indexOf
+    const array = this.state.items;
+    const index = array.indexOf(item);
+    array.splice(index, 1);
+    this.setState({
+        items: array
+    });
+}
+
+// add item to deleteTodo.bind(this, item)
+renderTodos() {
+    return this.state.items.map((item) => {
+        return (
+            <div className="list-group-item" key={item}>
+                {item} | <button onClick={this.deleteTodo.bind(this, item)}>X</button>
+            </div>    
+        );
+    });
+}
+
+render() {
+    return(
+        <div>
+            <h1 align="center">Todo list</h1>
+            <form className="form-row align-items-center">
+                <input 
+                    value={this.state.userInput} 
+                    type="text" 
+                    placeholder="Fill in an item"
+                    onChange={this.onChange.bind(this)}
+                    className="form-control mb-2"
+                />
+                <button 
+                    onClick={this.addTodo.bind(this)} 
+                    className="btn btn-primary"
+                >
+                    Add
+                </button>
+            </form>
+            <div className="list-group">
+                {this.renderTodos()}
+            </div>
+        </div>
+    );
+  }
 }
 
 export default App;
