201c601b711dce4dc02794625c269e0d71b67af6
SASSOUI Abdel <sassouiabd@gmail.com>
2019-07-27 23:42:14 +0200

    Updates

---
+++ precache-manifest.63197aa2846015167cbf75a66e6292c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09c0a96620f265c6eb63d4016456e0c6",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "224fa131d360bf76c9dd",
+    "url": "/ToDoList/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "0440f5c61b89d51833e3",
+    "url": "/ToDoList/static/js/2.97294558.chunk.js"
+  },
+  {
+    "revision": "224fa131d360bf76c9dd",
+    "url": "/ToDoList/static/js/main.6035d12c.chunk.js"
+  },
+  {
+    "revision": "6a71c43a298729cf07dd",
+    "url": "/ToDoList/static/js/runtime~main.67fadf76.js"
+  }
+]);
